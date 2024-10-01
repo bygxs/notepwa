6d986492840671e25c371a311460a6e10d96e746
Xiabher <99346955+Xiabher@users.noreply.github.com>
2024-10-01 15:17:45 +0200

    made a lot of changes everywhere

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,34 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWA from "next-pwa";
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // cacheOnFrontEndNav: true,
+    // aggressiveFrontEndNavCaching: true,
+    // reloadOnOnline: true,
+    // swcMinify: true,
+    // dest: "public",
+    // fallbacks: {
+    //     //image: "/static/images/fallback.png",
+    //     document: "/offline", // if you want to fallback to a custom page rather than /_offline
+    //     // font: '/static/font/fallback.woff2',
+    //     // audio: ...,
+    //     // video: ...,
+    // },
+    // workboxOptions: {
+    //     disableDevLogs: true,
+    // },
+    // // ... other options you like
+
+    reactStrictMode: true, // Enable React strict mode for improved error handling
+    swcMinify: true, // Enable SWC minification for improved performance
+    compiler: {
+        removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
+    },
+};
+
+export default withPWA({
+    dest: "public", // destination directory for the PWA files
+    disable: process.env.NODE_ENV === "development", // disable PWA in the development environment
+    register: true, // register the PWA service worker
+    skipWaiting: true, // skip waiting for service worker activation
+})(nextConfig);
